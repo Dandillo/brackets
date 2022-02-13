c60457b9866a7b3ea6490d050de514fc3078ab88
Danil <75947542+Dandillo@users.noreply.github.com>
2022-02-13 19:44:34 +0500

    init: start task, 9 tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let closeBracket = '';
+  let openBracket = '';
+  let res = 0;
+  let isCorrect = true;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    closeBracket = bracketsConfig[i][0];
+    openBracket = bracketsConfig[i][1];
+    res = 0;
+    for (let bracket in str) {
+      
+      if (bracket === openBracket) {
+        res++;
+      }
+      else if (bracket === closeBracket) {
+        res--;
+      }
+    }
+    isCorrect = isCorrect & (res === 0)
+  }
+  return isCorrect;
 }
